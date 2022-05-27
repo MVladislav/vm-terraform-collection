0bc15ef504f9ae7e89b8613b58478c7ac86ca5ab
MVladislav <MVladislav>
2022-05-27 02:57:19 +0200

    feat: tf base deploy add longhorn

---
+++ files/kubernetes/0-base/c-helm-longhorn.tf
@@ -0,0 +1,75 @@
+resource "kubernetes_namespace" "longhorn" {
+
+  # depends_on = [time_sleep.wait_for_kubernetes]
+
+  metadata {
+    name = "longhorn"
+  }
+
+}
+# ------------------------------------------------------------------------------
+
+resource "helm_release" "longhorn" {
+
+  depends_on = [kubernetes_namespace.longhorn]
+
+  name      = "longhorn"
+  namespace = "longhorn"
+
+  repository = "https://charts.longhorn.io"
+  chart      = "longhorn"
+  version    = "1.2.4"
+
+  # DEFAULT setup
+  set {
+    name  = "persistence.defaultClassReplicaCount"
+    value = 3
+  }
+  set {
+    name  = "persistence.reclaimPolicy"
+    value = "Retain"
+  }
+
+  # INGRESS setup
+  set {
+    name  = "ingress.enabled"
+    value = true
+  }
+  set {
+    name  = "ingress.ingressClassName"
+    value = var.kube_cert_ingress_class
+  }
+  set {
+    name  = "ingress.annotations.cert-manager\\.io/cluster-issuer"
+    value = var.kube_cert_cluster_issuer
+  }
+  set {
+    name  = "ingress.annotations.traefik\\.ingress\\.kubernetes\\.io/router\\.entrypoints"
+    value = "websecure"
+  }
+  set {
+    name  = "ingress.tls"
+    value = true
+  }
+  set {
+    name  = "ingress.tlsSecret"
+    value = "longhorn"
+  }
+  set {
+    name  = "ingress.host"
+    value = "longhorn.home.local"
+  }
+
+}
+
+# ------------------------------------------------------------------------------
+
+resource "time_sleep" "wait_for_longhorn" {
+
+  depends_on = [helm_release.longhorn]
+
+  create_duration = "30s"
+
+}
+
+# ------------------------------------------------------------------------------
